0a66912daa385e6c57fc71b87b6c2c206d5ef010
Alex Caza <alex@alexcaza.com>
2023-08-18 14:35:45 -0400

    Initial commit

---
+++ .wezterm.lua
@@ -0,0 +1,33 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'Kanagawa (Gogh)'
+config.font_size = 16
+config.font = wezterm.font 'CommitMono'
+config.keys = {
+    {
+        key = 'w',
+        mods = "CMD",
+        action = wezterm.action.CloseCurrentPane {confirm = true}
+    },
+    {
+        key = 'W',
+        mods = "CMD",
+        action = wezterm.action.CloseCurrentTab {confirm = true}
+    }
+}
+
+-- and finally, return the configuration to wezterm
+return config
---
+++ link.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ln .wezterm.lua ~/.wezterm.lua
