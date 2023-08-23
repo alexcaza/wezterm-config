0e47ca4f0e3705cee0eb90c2e5f8bd00fb0febcd
Alex Caza <alex@alexcaza.com>
2023-08-23 15:41:59 -0400

    Update link.sh
    Now symlinks weztermocil to /usr/local/bin
    Also creates symlink to config files in dir -> ~/.weztermocil

--- link.sh
+++ link.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 ln wezterm.lua ~/.wezterm.lua
+
+mkdir -p ~/.weztermocil
+
+ln -s $(pwd)/weztermocil/configs/* ~/.weztermocil/
+
+chmod +x $(pwd)/weztermocil/weztermocil.sh
+
+echo "Linking weztermocil... Might need sudo priviledges"
+sudo ln weztermocil/weztermocil.sh /usr/local/bin/weztermocil
